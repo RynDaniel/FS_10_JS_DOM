2a1e173878522e8d1e93a5c3a1d5342f3fd5d7dd
RynDaniel <ryan@clarusway.com>
2022-02-05 15:02:14 +0300

    last commit

--- signup.js
+++ signup.js
@@ -2,6 +2,9 @@
 let firstNameById = document.getElementById("first_name");
 console.log(firstNameById);
 
+let lastNameById = document.getElementById("last_name");
+console.log(lastNameById);
+
 // let firstNameByClassName = document.getElementsByClassName("input--style-4")[0];
 // console.log(firstNameByClassName);
 
@@ -29,9 +32,36 @@ console.log(firstNameById);
 
 // property vs. attribute
 
-console.log(firstNameById.getAttribute("id"));
-console.log(firstNameById.getAttribute("class"));
+// console.log(firstNameById.getAttribute("id"));
+// console.log(firstNameById.getAttribute("class"));
 // firstNameById.setAttribute("class", "redColorClass");
 // firstNameById.setAttribute("type", "radio");
 
 //innerHTML, innerText, textContent, (input=>value)
+// let headerDiv = document.getElementById("headerId");
+// console.log("innerHTML=>", headerDiv.innerHTML);
+// console.log("innerText=>", headerDiv.innerText);
+// console.log("textContent=>", headerDiv.textContent);
+
+// firstNameById.value = "Ryan";
+firstNameById.setAttribute("value", "Ryan")
+lastNameById.value = "Daniel";
+
+// // 🔥🔥🔥🔥🔥 FULL NAME(camelcase =>RYAN DANIEL) and E-MAIL(lovercase => ryan.daniel@clarusway.com) CHALLENGE  🔥🔥🔥🔥🔥
+
+let fullName = document.querySelector("#full_name");
+fullName.value = `${firstNameById.value.toUpperCase()} ${lastNameById.value.toUpperCase()}`;
+
+let email = document.getElementById("email");
+email.value = `${firstNameById.value.toLowerCase()}.${lastNameById.value.toLowerCase()}@cw.com`;
+
+//create logo element
+let headerDiv = document.getElementById("headerId");
+let logo = document.createElement("img");
+console.log(logo);
+logo.setAttribute("src", "img/logo.png");
+logo.setAttribute("class", "headerLogo");
+
+// headerDiv.append(logo); 
+// headerDiv.appendChild(logo); 
+headerDiv.innerHTML += '<img src="img/logo.png" alt="Bank logo" class="nav__logo" id="logo" />';

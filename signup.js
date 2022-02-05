c8440ad92c2396da4c9f9c3839a0163dfe9a9061
RynDaniel <ryan@clarusway.com>
2022-02-05 13:29:22 +0300

    second lesson commit

---
+++ signup.js
@@ -0,0 +1,28 @@
+// DOM Accessing
+let firstNameById = document.getElementById("first_name");
+console.log(firstNameById);
+
+let firstNameByClassName = document.getElementsByClassName("input--style-4")[0];
+console.log(firstNameByClassName);
+
+let firstNameByName = document.getElementsByName("first_name")[0];
+console.log(firstNameByName);
+
+let firstNameBySelectorId = document.querySelector("#first_name");
+console.log(firstNameBySelectorId);
+
+let firstNameBySelectorClass = document.querySelector(".input--style-4");
+window.console.log(firstNameBySelectorClass);
+
+// let firstNameBySelectorName = document.querySelector("[name='first_name']");
+// console.log(firstNameBySelectorName);
+
+// let firstNameBySelectorInputName = document.querySelector("input[name=first_name]");
+// console.log(firstNameBySelectorInputName);
+
+let firstNameBySelectorAllClass = document.querySelectorAll(".input--style-4")[0];
+window.console.log(firstNameBySelectorAllClass);
+
+// getElementsByClassName => Array.from(HTMLCollection).arrayFunc()
+// getElementsByName => NodeList.arrayFunc()
+// querySelectorAll => NodeList.arrayFunc()

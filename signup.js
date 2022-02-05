ea046a639b0455e4655ab087f6d63bd0c03ea2b8
RynDaniel <ryan@clarusway.com>
2022-02-05 13:48:11 +0300

    second lesson last commit

--- signup.js
+++ signup.js
@@ -2,17 +2,17 @@
 let firstNameById = document.getElementById("first_name");
 console.log(firstNameById);
 
-let firstNameByClassName = document.getElementsByClassName("input--style-4")[0];
-console.log(firstNameByClassName);
+// let firstNameByClassName = document.getElementsByClassName("input--style-4")[0];
+// console.log(firstNameByClassName);
 
-let firstNameByName = document.getElementsByName("first_name")[0];
-console.log(firstNameByName);
+// let firstNameByName = document.getElementsByName("first_name")[0];
+// console.log(firstNameByName);
 
-let firstNameBySelectorId = document.querySelector("#first_name");
-console.log(firstNameBySelectorId);
+// let firstNameBySelectorId = document.querySelector("#first_name");
+// console.log(firstNameBySelectorId);
 
-let firstNameBySelectorClass = document.querySelector(".input--style-4");
-window.console.log(firstNameBySelectorClass);
+// let firstNameBySelectorClass = document.querySelector(".input--style-4");
+// window.console.log(firstNameBySelectorClass);
 
 // let firstNameBySelectorName = document.querySelector("[name='first_name']");
 // console.log(firstNameBySelectorName);
@@ -20,9 +20,18 @@ window.console.log(firstNameBySelectorClass);
 // let firstNameBySelectorInputName = document.querySelector("input[name=first_name]");
 // console.log(firstNameBySelectorInputName);
 
-let firstNameBySelectorAllClass = document.querySelectorAll(".input--style-4")[0];
-window.console.log(firstNameBySelectorAllClass);
+// let firstNameBySelectorAllClass = document.querySelectorAll(".input--style-4")[0];
+// window.console.log(firstNameBySelectorAllClass);
 
 // getElementsByClassName => Array.from(HTMLCollection).arrayFunc()
 // getElementsByName => NodeList.arrayFunc()
 // querySelectorAll => NodeList.arrayFunc()
+
+// property vs. attribute
+
+console.log(firstNameById.getAttribute("id"));
+console.log(firstNameById.getAttribute("class"));
+// firstNameById.setAttribute("class", "redColorClass");
+// firstNameById.setAttribute("type", "radio");
+
+//innerHTML, innerText, textContent, (input=>value)
